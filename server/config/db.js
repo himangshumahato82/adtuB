dde3fc3fb40d18190268f435c1191bdfdf565fff
himangshumahato82 <himangshumahato82@gmail.com>
2023-06-06 06:30:46 +0530

    done

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
-const mognURL ="mongodb+srv://himangshumahato82:himu82@cluster0.o1tg8gs.mongodb.net/?retryWrites=true&w=majority";
+const dotenv = require("dotenv");
+
+dotenv.config();
+const MONGO_URL=process.env.MONGO_URL
 const ConnectDB = () => {
   mongoose
-    .connect(mognURL, { useNewUrlParser: true })
+    .connect(MONGO_URL, { useNewUrlParser: true })
     .then((res) => console.log(res.connection.db.databaseName))
     .catch((error) => console.log(error));
 };
